2f9e4ac81db9e4c10e1311e69bb36b10e3441226
Didenko Andre <didenko.andre@gmail.com>
2017-02-24 03:12:11 +0300

    Update server.py

--- bot.py
+++ bot.py
@@ -5,7 +5,6 @@ from telegram import Document, Bot
 from telegram.error import NetworkError, Unauthorized, TelegramError
 import time
 
-
 def send_file(bot, filename, chat_id, _type, caption, **kwargs):
 
     def upload(file_id):
@@ -22,11 +21,20 @@ def send_file(bot, filename, chat_id, _type, caption, **kwargs):
     return upload_from_disk()
 
 def start(bot, update):
-    bot.sendMessage(chat_id=update.message.chat_id, text = "Я бот, вижу, вы хотите поботать ГОС?")
+    bot.sendMessage(chat_id=update.message.chat_id, text = "Я бот, вижу, вы хотите поботать ГОС?\n\
+	Для ознакомления со списком возможных команд, запросите /help")
+	
+def help(bot, update):
+	bot.sendMessage(chat_id=update.message.chat_id, text = "Список команд данного бота:\n\
+	/book -- получить последнюю версию ГОСБука в pdf-формате\n\
+	/subscribe -- подписаться на рассылку об обновлениях ГОСБука, чтобы автоматически получать новые версии ГОСБука, а также читать новости, касающиеся ГОСа\n\
+	/unsubscribe -- отписаться от выше указанной новостной рассылки\n\
+	/help -- вывести список команд данного бота\n\n\
+	PS. Если я как-то неправильно работаю или у тебя есть интересные предложения по улучшению меня, то напиши, пожалуйста, моему создателю @didenko_andre")
 
 def getbook(bot, update):
-    send_file(bot, "/home/ec2-user/GOS_book/_main.pdf", update.message.chat_id, None,
-                      caption="Вот ваша книга")
+    send_file(bot, "/home/ec2-user/GOS_book/GOSBook.pdf", update.message.chat_id, None,
+                      caption="Вот последняя версия ГОСбука")
 
 def get_subscribers():
     with open('subscribers.txt', 'r') as db:
@@ -55,7 +63,10 @@ def subscribe(bot, update):
         bot.sendMessage(chat_id=id, text = "Вы уже являетесь подписчиком!")
         return None
     add_subscriber(id)	
-    bot.sendMessage(chat_id=id, text = "Вы успешно подписались на рассылку о обновлениях ГОСбука!")
+    bot.sendMessage(chat_id=id, text = "Вы успешно подписались на рассылку об обновлениях ГОСбука!")
+	
+	bot.sendMessage(chat_id=didenko_andre_id, text = \
+	"На твоего бота подписался еще один человек, теперь у бота " + str(len(get_subscribers())) + "подписчиков.")
 
 def unsubscribe(bot, update):
     id = update.message.chat_id
@@ -63,10 +74,18 @@ def unsubscribe(bot, update):
         bot.sendMessage(chat_id=id, text = "Вы не являетесь подписчиком!")
         return None
     del_subscriber(id)
-    bot.sendMessage(chat_id=id, text = "Вы прекратили свою подписку о обновлениях ГОСбука!")
+    bot.sendMessage(chat_id=id, text = "Вы прекратили свою подписку на рассылку об обновлениях ГОСбука!")
+	
+	bot.sendMessage(chat_id=didenko_andre_id, text = \
+	"От твоего бота кто-то отписался, теперь у бота " + str(len(get_subscribers())) + "подписчиков.")
 
 if __name__ == '__main__':
-	TOKEN = '305103696:AAGtt_a0EjkvU7F9ySpi1Snn6eHkMgWRW0U'
+	with open('GOSBook_Bot_token') as file1:
+		TOKEN = file.read().strip()
+		
+	with open('didenko_andre_id') as file2:
+		didenko_andre_id = file.read().strip()
+	
 	updater = Updater(token = TOKEN)
 	dispatcher = updater.dispatcher
 
@@ -75,6 +94,8 @@ if __name__ == '__main__':
 	# Command handlers
 	start_handler = CommandHandler('start', start)
 	dispatcher.add_handler(start_handler)
+	help_handler = CommandHandler('help', help)
+	dispatcher.add_handler(help_handler)
 	book_handler = CommandHandler('book', getbook)
 	dispatcher.add_handler(book_handler)
 	subscribe_handler = CommandHandler('subscribe',subscribe)
--- broadcast.py
+++ broadcast.py
@@ -4,9 +4,11 @@ from telegram import Document, Bot
 from telegram.error import NetworkError, Unauthorized, TelegramError
 import time
 
-TOKEN = '305103696:AAGtt_a0EjkvU7F9ySpi1Snn6eHkMgWRW0U'
+with open('GOSBook_Bot_token') as file:
+	TOKEN = file.read().strip()
+	
 bot = Bot(token = TOKEN)
 
 for id in get_subscribers():
-    send_file(bot, "/home/ec2-user/GOS_book/_main.pdf", id, None, caption="Получайте обновление")
+    send_file(bot, "/home/ec2-user/GOS_book/GOSBook.pdf", id, None, caption="Вышла новая версия ГОСбука! Получайте свежую версию книги.")
     time.sleep(1)
